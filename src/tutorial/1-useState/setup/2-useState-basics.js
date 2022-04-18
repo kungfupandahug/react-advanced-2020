9db847d328e6e1c698907bcdfde4410648e2487e
Inthu <kungfupandahug@gmail.com>
2022-04-18 14:32:36 +0200

    feat: useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
 
+/*
+    - Hooks will start with 'use...'
+    - component name we invoke these hooks in, must be in uppercase
+    - the hook must be in the function / component body
+    - the hook cannot be called conditionally
+ */
+
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('random title');
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText('hello world');
+    } else {
+      setText('random title');
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>change title</button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,49 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  const removeItem = (id) => {
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
+    setPeople((oldPeople) => {
+      let newPeople = oldPeople.filter((person) => person.id !== id);
+      return newPeople;
+    });
+  };
+  return (
+    <>
+      {
+        people.map((person) => {
+          const { id, name } = person;
+          return <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>;
+        })
+      }
+      {/* <button className='btn' onClick={setPeople([])}>clear items</button> */}
+      <button className='btn' onClick={() => setPeople([])}>clear items</button>
+    </>
+  );
 };
 
 export default UseStateArray;
+
+/*
+
+  The following corresponds to React.Fragment and a div
+
+  return (
+    <>
+      {
+        ...
+      }
+    </>
+  )
+
+*/
+
+/*
+  onClick={setPeople([])} invokes immediately
+  onClick={() => setPeople([])} invokes only when the button is clicked
+*/
