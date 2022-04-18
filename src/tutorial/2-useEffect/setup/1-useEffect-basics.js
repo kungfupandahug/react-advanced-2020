08848c27f65eba80cd58841750b28125e625b02b
Inthu <kungfupandahug@gmail.com>
2022-04-18 14:33:25 +0200

    feat: useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,44 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log('call useEffect');
+    if (value > 0) {
+      document.title = `New Messages(${value})`;
+    }
+  });
+
+  useEffect(() => {
+    console.log('hello world');
+  }, []);
+
+  // if you want useEffect to only run in the initial render, send
+  // an empty array as dependency list
+  // useEffect(() => {
+  //   console.log('call useEffect');
+  //   if (value > 0) {
+  //     document.title = `New Messages(${value})`;
+  //   }
+  // }, []);
+
+  // if you want useEffect to run only after changes in a certain set
+  // set of data, add that to the dependency list. This way you'll
+  // enforce useEffect to stay still during every re-render and only
+  // kick in at certain points (like when 'value' here is updated)
+  // useEffect(() => {
+  //   console.log('call useEffect');
+  //   if (value > 0) {
+  //     document.title = `New Messages(${value})`;
+  //   }
+  // }, [value]);
+
+  console.log('render component');
+  return <>
+    <h1>{value}</h1>
+    <button className='btn' onClick={() => setValue(value + 1)}>click me</button>
+  </>;
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,45 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+
+  // useState preserves the value and triggers the re-render
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+
+    setUsers(users);
+    // this will trigger re-render while inside a re-render from
+    // useEffect... re-render loop will occur :(
+    // need to either add a dependency list to useEffect, and use
+    // setUsers here, OR
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  // useEffect cannot be async! it cannot return a promise!!
+  // useEffect(async ()=>{...}); // big nono!!
+  // can set up a sync await inside the function, or separat
+
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return <li key={id}>
+            <img src={avatar_url} alt={login} />
+            <div>
+              <h4>{login}</h4>
+              <a href={html_url}>profile</a>
+            </div>
+          </li>
+        })}
+      </ul>
+    </>
+  )
 };
 
 export default UseEffectFetchData;
