9fbd017f89ca971ff9cc9a6587197bb9bc77496d
Inthu <kungfupandahug@gmail.com>
2022-04-18 14:36:23 +0200

    feat: custom hooks

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,41 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
-export const useFetch = () => {};
+// not allowed to use hooks inside the regular function.
+// the function has to be a component or a custom hook.
+// has to begin with 'use...'
+// export const useFetch = (url) => {
+//   const [loading, setLoading] = useState(true)
+//   const [products, setProducts] = useState([])
+
+//   const getProducts = async () => {
+//     const response = await fetch(url)
+//     const products = await response.json()
+//     setProducts(products)
+//     setLoading(false)
+//   }
+
+//   useEffect(() => {
+//     getProducts()
+//   }, [url])
+
+//   return { loading, products };
+// };
+
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([])
+
+  const getProducts = useCallback(async () => {
+    const response = await fetch(url)
+    const products = await response.json()
+    setProducts(products)
+    setLoading(false)
+  }, [url]);
+
+  useEffect(() => {
+    getProducts()
+  }, [url, getProducts])
+
+  return { loading, products };
+};
