621e0e44f33b73e597199a18e5e0880a6b9a944a
Inthu <kungfupandahug@gmail.com>
2022-04-18 14:34:06 +0200

    feat: conditional rendering

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,52 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('panda');
+  const [isError, setIsError] = useState(false);
 
-  return <h2>short circuit</h2>;
+  // falsy first argument ('') in || expression will return the 'next' truthy value i.e. 'hello world' here
+  // truthy first argument ('k') in || exp will return the first argument 
+  const firstValue = text || 'hello world';
+
+  // any falsy argument in && exp will result in nothing
+  // all truthy arguments in && exp will return the last argument
+  const secondValue = text && 'hello world';
+
+  // examples on how truthy and falsy expressions work
+  // return (
+  //   <>
+  //     <h1>{firstValue}</h1>
+  //     <h1>value : {secondValue}</h1>
+  //   </>
+  // )
+
+  // This code will not work, but we want this type of logic.
+  // Need some kind of conditional mechanism when operating with
+  // single return statement
+  // return (
+  //   <>
+  //     {if(){console.log('hello world')}}
+  //   </>
+  // );
+
+  return (
+    <>
+      <h1>{text || 'john doe'}</h1>
+      {/* {text && <h1>hello world</h1>} */}
+      {/* {!text && <h1>hello world</h1>} */}
+
+      <button className='btn' onClick={() => setIsError(!isError)}>toggle error</button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
+    </>
+  );
+
 };
 
 export default ShortCircuit;
